3aaeb7150682ba2705de890c7abfc11e08bc8b13
ernes7 <ernechelsea@gmail.com>
2020-08-25 09:15:18 -0700

    fixed failures

--- spec/satisfy_matcher_spec.rb
+++ spec/satisfy_matcher_spec.rb
@@ -1,6 +1,5 @@
-RSpec.describe 'satisfy matcher' do
-  # subject { 'racecar' }
-  subject { 'racecars' }
+describe 'satisfy matcher' do
+  subject { 'racecar' }
 
   it 'is a palindrome' do
     expect(subject).to satisfy { |value| value == value.reverse  }
